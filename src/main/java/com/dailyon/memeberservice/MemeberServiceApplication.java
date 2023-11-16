7f904e4422807b32bbb3d316c9b199488e62810d
kkh <rlghks131@korea.ac.kr>
2023-11-16 15:04:36 +0900

    first commit

---
+++ src/main/java/com/dailyon/memeberservice/MemeberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dailyon.memeberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeberServiceApplication.class, args);
+    }
+
+}
